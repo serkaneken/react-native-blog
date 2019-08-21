911c7a64390e66feaa372bab8b25ee2d85aba335
serkaneken <serkanatwork@gmail.com>
2019-08-21 15:25:41 +0300

    Fix bad repo

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import IndexScreen from './src/screens/IndexScreen';
+import { Provider } from './src/context/BlogContext';
+import ShowScreen from './src/screens/ShowScreen';
+import CreateScreen from './src/screens/CreateScreen';
+import EditScreen from './src/screens/EditScreen';
+
+
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
+    Show:ShowScreen,
+    Create:CreateScreen,
+    Edit:EditScreen
+  },
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs'
+    }
+  }
+);
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (
+    <Provider>
+      <App />
+    </Provider>
+  );
+};
---
+++ src/context/BlogContext.js
@@ -0,0 +1,57 @@
+import createDataContext from './createDataContext';
+
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'edit_blogpost':
+            return state.map(blogPost => {
+                return blogPost.id === action.payload ? action.payload : blogPost;
+            });
+        case 'delete_blogpost':
+            return state.filter(blogPost => blogPost.id !== action.payload);
+        case 'add_blogpost':
+            return [
+                ...state,
+                {
+                    id: Math.floor(Math.random() * 9999),
+                    title: action.payload.title,
+                    content: action.payload.content
+                }
+            ];
+        default:
+            return state;
+    }
+};
+
+const addBlogPost = dispatch => {
+    return (title, content, callback) => {
+        dispatch({ type: 'add_blogpost', payload: { title, content } });
+        if (callback) {
+            callback();
+        }
+    };
+};
+
+const deleteBlogPost = dispatch => {
+    return id => {
+        dispatch({ type: 'delete_blogpost', payload: id });
+    };
+};
+
+const editBlogPost = dispatch => {
+    return (id, title, content, callback) => {
+        dispatch({
+            type: 'edit_blogpost',
+            payload: { id, title, content }
+        });
+        if (callback) {
+            callback();
+
+        }
+    };
+}
+
+export const { Context, Provider } = createDataContext(
+    blogReducer,
+    { addBlogPost, deleteBlogPost, editBlogPost },
+    [{ title: 'TEST', content: 'test', id: 1 }]
+);
